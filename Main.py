0fa61daa005292b585e622323a6ac5ac83cb6eea
Mosser <loic.mosser@ens-rennes.fr>
2018-05-28 09:14:09 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,8 +3,9 @@
 from Import_datas import Load_Arff_File
 from Model_definition import define_model_conv_mult,train_model
 from Shapelet_list_creation import Grabocka_Shapelets
-from keras.callbacks import EarlyStopping#,CSVLogger
-from Special_Callbacks import Plot_acc_Losses_end_train,Plot_acc_Losses_during_train
+from keras.callbacks import EarlyStopping
+from Special_Callbacks import Plot_acc_Losses_end_train,Plot_acc_Losses_during_train,Shapelet_Analysis
+from Regularisation import l2_reg
 
 # %% import des données du dataset en trois parties distinctes piochées aléatoirement dans celui ci avec 72 % du dataset dans le train set,
 # 28 % dans le test set et 15 % du train set dans le validation set
@@ -18,15 +19,15 @@ Shapelets = Grabocka_Shapelets()    # Heuristique de Grabocka
 
 # %% definition du modèle et des Callbacks
 
-Model = define_model_conv_mult(Shapelets,None)  # definition du modèle sans régulation
+Model = define_model_conv_mult(Shapelets,l2_reg)  # definition du modèle sans régulation
 
-# Plot_acc_Losses_end_train = Plot_acc_Losses_end_train(filename = "Train_results.png")
-Plot_acc_Losses_during_train = Plot_acc_Losses_during_train(filename = "Train_results.png",donnee_suivi="Acc")
-Early_stoping = EarlyStopping(monitor='loss', min_delta=0, patience=3, verbose=0, mode='auto',)   # Early_stopping with patience on the loss metric variable
-#CSVLogger = CSVLogger('donnees_apprentissage.csv', separator=',', append=False)
-Callbacks = [Early_stoping,Plot_acc_Losses_during_train] # definition des Callbacks
+Plot_acc_Losses_end_train = Plot_acc_Losses_end_train(filename = "Train_results.png")
+Plot_acc_Losses_during_train = Plot_acc_Losses_during_train(donnee_suivi="Acc") # donnee_suivi="Loss"
+Shapelet_Analysis = Shapelet_Analysis(Model = Model,X_train = X_train,num_shapelet=0,num_couche_conv = 1)
+Early_stoping = EarlyStopping(monitor='loss', min_delta=0, patience=2, verbose=0, mode='auto',)   # Early_stopping with patience on the loss metric variable
+Callbacks = [Early_stoping,Shapelet_Analysis] # definition des Callbacks
 
 # %% definition de la compilation et de l'apprentissage
 
 Model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-(model,accuracy,History) = train_model(Model ,X_train ,Y_train ,X_test ,Y_test ,X_val ,Y_val ,nb_epoch = 5 ,Callbacks = Callbacks)
+(model,accuracy,History) = train_model(Model ,X_train ,Y_train ,X_test ,Y_test ,X_val ,Y_val ,nb_epoch = 400 ,Callbacks = Callbacks,ver = 1)
